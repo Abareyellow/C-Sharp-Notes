9da42d239aea53c582c854ec5115663e3a5596c9
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-11-08 20:06:11 +0000

    started learning C-Sharp

---
+++ class.cs
@@ -0,0 +1,15 @@
+public class Employee {
+    public string LastName;
+    public int Id;
+    public int Age;
+    public string FirstName;
+
+    public Employee() {
+
+    }
+
+    public Employee(string firstName, string lastName) {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
