7bee31cd9553ef2f8d13ef8e37277e155dcb029e
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2025-03-07 20:47:37 +0000

    Done

--- obj/Debug/net8.0/C-Sharp-Notes.AssemblyInfo.cs
+++ obj/Debug/net8.0/C-Sharp-Notes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-Sharp-Notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7c9455c044ed220ccbde08639ebc0b781bb3655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0e3c23f5e357667d1291d5e3893b596003c0930")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-Sharp-Notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-Sharp-Notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
